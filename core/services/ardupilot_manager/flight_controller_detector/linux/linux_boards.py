074136605cfa28955dd8785250fbe7f2e650aa58
nicoschmdt <nicoschmdt@proton.me>
2025-07-24 19:25:49 -0300

    ardupilot_manager: create shortcut for script directory when using linux

--- core/services/ardupilot_manager/flight_controller_detector/linux/linux_boards.py
+++ core/services/ardupilot_manager/flight_controller_detector/linux/linux_boards.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import List, Type
 
 from smbus2 import SMBus
@@ -25,6 +27,10 @@ class LinuxFlightController(FlightController):
             return True
         except OSError:
             return False
+
+    def setup(self) -> None:
+        os.makedirs("/root/.config/ardupilot-manager/firmware/scripts", exist_ok=True)
+        os.symlink("/root/.config/ardupilot-manager/firmware/scripts", "/shortcuts/lua_scripts")
 
     @classmethod
     def get_all_boards(cls) -> List[Type["LinuxFlightController"]]:
